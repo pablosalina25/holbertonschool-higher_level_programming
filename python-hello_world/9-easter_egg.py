3963f90926c0ac7f84de936f2c2bbd7284449ffd
pablosalina25 <pablosalina25@gmail.com>
2024-01-22 15:35:16 -0800

    task 7

---
+++ python-hello_world/9-easter_egg.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str1 = " is better than "
+str2 = "If the implementation is "
+print(f Beautiful {str1} ugly."\n" Explicit {str1} implicit."\n" Simple {str1} complex.Complex {str1} complicated. "\n"
