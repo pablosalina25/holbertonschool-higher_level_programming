7085bc2969ae676f3c1f10f0774c8589d99d6faa
pablosalina25 <pablosalina25@gmail.com>
2024-01-22 15:42:13 -0800

    task 7

--- python-hello_world/9-easter_egg.py
+++ python-hello_world/9-easter_egg.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-str1 = " is better than "
-str2 = "If the implementation is "
-print(f Beautiful {str1} ugly."\n" Explicit {str1} implicit."\n" Simple {str1} complex.Complex {str1} complicated. "\n"
+print("The Zen of Python, by Tim Peters")
