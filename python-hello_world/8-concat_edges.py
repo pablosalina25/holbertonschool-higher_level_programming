156f3f22ea9bab3d82c9c974fcff41c53ec8b40a
pablosalina25 <pablosalina25@gmail.com>
2024-01-22 15:16:41 -0800

    task 6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = (str[39:67] + str[8:9] + str[105:111] + str[12:13] + str[0:6])
+str = (str[39:67] + str[105:111] + str[12:13] + str[0:6])
 print(str)
