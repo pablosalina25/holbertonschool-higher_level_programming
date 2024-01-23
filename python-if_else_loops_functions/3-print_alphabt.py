bd3453e13633d04be9ebb71834e8f846ba6a2aec
pablosalina25 <pablosalina25@gmail.com>
2024-01-23 10:05:43 -0800

    task 2

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for alp in range(97, 123):
-    if alp == 101:
-        alp += 1
-    elif alp == 113:
-        alp +=1 
-        print("{}".format(chr(alp)), end="")
+    if alp == 101 or alp == 113:
+        continue
+    print("{}".format(chr(alp)), end="")
