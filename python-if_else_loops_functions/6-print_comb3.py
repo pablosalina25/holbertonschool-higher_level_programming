8a88ab4956bab0fa84efc3d4069f225aaaf6c46c
pablosalina25 <pablosalina25@gmail.com>
2024-01-24 05:24:11 -0800

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 for num1 in range(0, 10):
-    for num2 in range(10, 0, -1):
-        print("{} {}".format(num1, num2))
+    for num2 in range(num1 + 1, 10):
+        print("{:02d}, {:02d}".format(num1, num2))
