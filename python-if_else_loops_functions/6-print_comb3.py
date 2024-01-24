2c767b82077e7c7f18c3b065a1694817562abeef
pablosalina25 <pablosalina25@gmail.com>
2024-01-24 05:30:33 -0800

    task 7

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
 for num1 in range(0, 10):
     for num2 in range(num1 + 1, 10):
-        print("{:02d}, {:02d}".format(num1, num2))
+        if num1 != 8:
+            print("{}{}".format(num1, num2), end=", ")
+        else:
+            print("{}{}".format(num1, num2))
