bd8372a12148b040b65d5582fe87af99a79443a7
pablosalina25 <pablosalina25@gmail.com>
2024-01-26 12:32:17 -0800

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    full = 0
+    if len(sys.argv) > 0:
+        for n in arguments:
+            total += int(n)
+        print(full)
+    else:
+        print(full)
