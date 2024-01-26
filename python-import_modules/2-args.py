bca50e7eae2e269cedf3055b9f6f623462d26548
pablosalina25 <pablosalina25@gmail.com>
2024-01-26 12:16:44 -0800

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     if len(sys.argv) == 1:
         print("0 arguments.")
     elif len(sys.argv) == 2:
-        print("1 argument:{}".format(sys.argv[1]))
-    elif len(sys.argv) > 2:
-        print("{} arguments: {}".format(len(sys.argv), sys.argv[1:]))
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for p in range(1, len(sys.argv)):
+            print("{}: {}".format(n, sys.argv[n]))
