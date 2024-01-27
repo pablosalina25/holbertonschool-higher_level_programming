924b74c1353b322378b368504da635f8fa8436c7
pablosalina25 <pablosalina25@gmail.com>
2024-01-27 08:49:57 -0800

    1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0 or idx > len(my_list):
+        return ("None")
+    for j in range(0, len(my_list)):
+        if j == idx:
+            return my_list[i]
