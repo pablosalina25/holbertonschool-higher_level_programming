ba1332466805f57a4210858c3ac45b0130f96eb6
pablosalina25 <casuarioventas@gmail.com>
2024-01-30 15:27:35 +0000

    soaces

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,10 +2,10 @@
 def uniq_add(my_list=[]):
     new_list = set()
     total = 0
-    
+
     for num in my_list:
         if num not in new_list:
             total += num
             new_list.add(num)
-    
+
     return total
