6f2c7e2c73befd0189054fb941dd6229e461ad74
pablosalina25 <pablosalina25@gmail.com>
2024-01-30 13:01:13 -0800

    5-number_keys.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# This script defines a function to retrieve elements that are different between two sets.
 def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2
+    return (set_1 ^ set_2)
