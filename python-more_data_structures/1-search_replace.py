1838894c22d786b8c28ded999c1dcfaabecaface
pablosalina25 <casuarioventas@gmail.com>
2024-01-30 14:59:29 +0000

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-new_list = my_list.copy()
+    new_list = my_list.copy()
     for j in range(len(new_list)):
         if new_list[j] == search:
             new_list[j] = replace
