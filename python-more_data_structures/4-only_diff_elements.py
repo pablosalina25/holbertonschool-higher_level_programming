833fcd161bd51c7fb71a9e5df7241d4189d50dda
pablosalina25 <pablosalina25@gmail.com>
2024-01-30 12:47:06 -0800

    task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 # This script defines a function to retrieve elements that are different between two sets.
-
 def only_diff_elements(set_1, set_2):
     set_3 = set_1.symmetric_difference(set_2)
     return set_3
-
