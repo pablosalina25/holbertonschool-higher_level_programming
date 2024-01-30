5e44d45c4bf023cd03debcbb57f44a88c343e092
pablosalina25 <pablosalina25@gmail.com>
2024-01-30 05:48:47 -0800

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = [row[:] for row in matrix]
-    for line in newmatrix:
+    for line in new_matrix:
         for j in range(len(line)):
             line[j] = line[j] * line[j]
     return new_matrix
