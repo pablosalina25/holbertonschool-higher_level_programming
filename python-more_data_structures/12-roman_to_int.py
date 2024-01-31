50773c1759bffecd04d8009899cd352006a4fd8d
pablosalina25 <casuarioventas@gmail.com>
2024-01-31 13:15:28 +0000

    arreglado

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,6 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
-
     roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
     prev_value = 0
@@ -17,4 +16,3 @@ def roman_to_int(roman_string):
             total += current_value
         prev_value = current_value
     return total
-
