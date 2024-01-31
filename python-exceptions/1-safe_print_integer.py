a4c6616eea1dfc854b43f2132c6777a5d3a3cb37
pablosalina25 <casuarioventas@gmail.com>
2024-01-31 13:07:57 +0000

    fixed in

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
-       try:
+    try:
         print("{:d}".format(value))
-        return (True)
+        return True
     except ValueError:
-        return (False)
+        return False
