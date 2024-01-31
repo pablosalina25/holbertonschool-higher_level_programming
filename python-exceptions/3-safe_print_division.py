dcd66b3cf7774703eec24fde2717daf30c11949e
pablosalina25 <casuarioventas@gmail.com>
2024-01-31 14:02:19 +0000

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         res_div = a / b
-    except Except:
+    except (ZeroDivisionError, ValueError, TypeError):
         res_div = None
     finally:
         print("Inside result: {}".format(res_div))
