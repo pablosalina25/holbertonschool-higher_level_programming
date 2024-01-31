6aeb257e82b7c6a03a9be7b1e613d3aeded2b99b
pablosalina25 <casuarioventas@gmail.com>
2024-01-31 16:49:46 +0000

    Antes de que la  pc quedara sin bateria

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,8 +8,8 @@ def list_division(my_list_1, my_list_2, list_length):
         finished_list.append(0)
         print("out of range")
     except TypeError:
-        finished_list.append(0)
-        print("wrong type")
+            finished_list.append(0)
+            print("wrong type")
     except ZeroDivisionError:
         finished_list.append(0)
         print("division by 0")
