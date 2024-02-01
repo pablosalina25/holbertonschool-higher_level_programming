1cb2174d52a70b44a69ccb0016665919f77fb2fd
pablosalina25 <pablosalina25@gmail.com>
2024-02-01 11:48:01 -0800

    [1

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Square Class"""
+    def __init__(self, size):
+        """Initializing"""
+        self.__size = size
