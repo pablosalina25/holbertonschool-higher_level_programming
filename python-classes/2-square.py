8dea0c4569f73d432e49da829df7c3dec10a9749
pablosalina25 <pablosalina25@gmail.com>
2024-02-03 07:42:53 -0800

    fixed

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This is a program that creates
-	a class named Square, based on 1-square.py."""
+    a class named Square, based on 1-square.py."""
 
 
 class Square:
     """This is the Square Class"""
+
     def __init__(self, size=0):
         """Initializing the square size"""
         self.__size = size
@@ -12,3 +13,4 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+
