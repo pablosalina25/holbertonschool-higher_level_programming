153311d46154c7c01aa94fc2f9957bf309b163db
pablosalina25 <pablosalina25@gmail.com>
2024-02-03 07:59:32 -0800

    fixed

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# A class Square that defines a square by: (based on 2-square.py).
+
+ """A class Square that defines a square by: (based on 2-square.py)."""
 
 class Square:
     """This is the Square Class"""
