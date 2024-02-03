fa7466619e16c14c540458108bc3862fe3904132
pablosalina25 <pablosalina25@gmail.com>
2024-02-03 07:50:32 -0800

    fixed

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """A class Square that defines a square by: (based on 2-square.py)."""
 
+
 class Square:
     """This is the Square Class"""
+
     def __init__(self, size=0):
         """Initializing the square size"""
         self.__size = size
@@ -12,13 +14,13 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
-        if type(size) is not int:
+    def size(self, value):
+        if type(value) is not int:
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
         """Return the area of the square"""
