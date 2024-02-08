68ed7fa5df388e91bf67488f74ee86fc87cb918a
pablosalina25 <pablosalina25@gmail.com>
2024-02-08 07:24:53 -0800

    task 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 """
 This module defines a simple Rectangle class.
 """
 
+
 class Rectangle:
+
     """
     This class represents a rectangle.
     It doesn't contain any attributes or methods yet.
     """
+
     pass
