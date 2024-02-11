dc39a63eec5ffb58798b5dd3dc804af4bd97f440
pablosalina25 <pablosalina25@gmail.com>
2024-02-11 09:27:48 -0800

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+A function that returns an object (Python data structure)
+ represented by a JSON string:
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    This function returns an object (Python data structure)
+    represented by a JSON string.
+    """
+
+    return (json.loads(my_str))
