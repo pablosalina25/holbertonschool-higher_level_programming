ea53bc08660e8cc4d8464c810d163b5b5c4d5724
pablosalina25 <pablosalina25@gmail.com>
2024-02-17 14:11:05 -0800

    test

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module works as unittest for Rectangle class.
+"""
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+
+
+class testRectangle(unittest.TestCase):
+    """
+    This is a class to test the Rectangle class.
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
