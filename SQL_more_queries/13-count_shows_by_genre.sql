6149f6416a1a6e77717bc5a2f92d9e5aa7a006dc
pablosalina25 <casuarioventas@gmail.com>
2024-03-15 18:16:53 +0000

    13-count_shows_by_genre.sql

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 12-no_genre.sql)
+
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id)
+AS number_of_shows
+FROM tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_show_genres.genre_id ORDER BY number_of_shows DESC;
